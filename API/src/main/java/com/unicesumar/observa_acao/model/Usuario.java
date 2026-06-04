92c72e6f8e0678de46b6ccbb1ceaaf50d3cf62a9
Leonardo <leonardocbizerra@gmail.com>
2026-06-03 21:59:35 -0300

    refactor: ajusta validacoes e tratamento global de excecoes

--- API/src/main/java/com/unicesumar/observa_acao/model/Usuario.java
+++ API/src/main/java/com/unicesumar/observa_acao/model/Usuario.java
@@ -6,13 +6,18 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "usuarios")
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor
-public class Usuario {
+public class Usuario implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -43,4 +48,39 @@ public class Usuario {
 
     @Column(nullable = false)
     private Boolean ativo;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + tipoUsuario.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return Boolean.TRUE.equals(this.ativo);
+    }
 }
--- API/src/main/java/com/unicesumar/observa_acao/repository/UsuarioRepository.java
+++ API/src/main/java/com/unicesumar/observa_acao/repository/UsuarioRepository.java
@@ -3,5 +3,13 @@ package com.unicesumar.observa_acao.repository;
 import com.unicesumar.observa_acao.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Optional<Usuario> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByCpf(String cpf);
 }
