6b6692cb7b362a0594e8b7f1918c43d4e1058e52
CaduCarfi <157553755+CaduCarfi@users.noreply.github.com>
2026-06-03 21:57:27 -0300

    feat: Categoria Service e ajuste no Categoria Repository

--- API/src/main/java/com/unicesumar/observa_acao/repository/CategoriaRepository.java
+++ API/src/main/java/com/unicesumar/observa_acao/repository/CategoriaRepository.java
@@ -2,6 +2,14 @@ package com.unicesumar.observa_acao.repository;
 
 import com.unicesumar.observa_acao.model.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+    boolean existsByNome(String nome);
+
+    List<Categoria> findAllByAtivaTrue();
 }
